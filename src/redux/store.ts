0e140f869d85693925590d03844e92a48168fb96
HelgaTaleriko <89998178494@mail.ru>
2024-03-20 15:21:51 +0300

    add redux persist for to save data after page reload

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,17 +1,29 @@
 import {useDispatch, useSelector} from "react-redux";
-import {configureStore} from "@reduxjs/toolkit";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import {goodsSlice} from "./goods/slice";
 import {cartSlice} from "./cart/slice";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
 export const useAppSelector = useSelector.withTypes<RootState>()
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
+const persistConfig = {
+    key: "root",
+    storage,
+};
 
-// Create Redux store
-export const store = configureStore({
-    reducer: {
+const persistedReducer = persistReducer(
+    persistConfig,
+    combineReducers({
         goods: goodsSlice.reducer,
         cart: cartSlice.reducer,
-    },
+    })
+);
+
+export const store = configureStore({
+    reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
